e75fee3bccd5128324c6330ca8b395b558539cab
NoSinging <NoSinging@users.noreply.github.com>
2015-09-02 22:45:17 +1000

    Add player sprite

--- js/app.js
+++ js/app.js
@@ -2,8 +2,7 @@
 var Enemy = function(row) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.v = 100;
-    this.x = 0;
+    this.resetPosition();
     this.row = row;
 
     // The image/sprite for our enemies, this uses
@@ -17,7 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.v * dt;
+    if (this.x > 500) {
+        this.resetPosition();
+    };
+
+    this.x = this.x + this.velocity * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,29 +28,46 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.row*83-10);
 };
 
+// Rest the enemy x position and velocity
+Enemy.prototype.resetPosition = function() {
+    this.x = -300 * Math.random();
+    this.velocity = 100 * Math.random() + 100;
+};
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-//ToDo
+    this.col = 2;
+    this.row = 5;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(dt) {
 
 };
 
-Player.prototype.render = function() {};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.col*101, this.row*83-10)
+};
 
-Player.prototype.handleInput = function() {};
+Player.prototype.handleInput = function(action) {
+    if (action == 'up') {
+        this.row--;
+    };
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 allEnemies = [
-    new Enemy(1),
-    new Enemy(2),
-    new Enemy(3)
+    new Enemy(1), new Enemy(1),
+    new Enemy(2), new Enemy(2),
+    new Enemy(3), new Enemy(3)
     ];
 
 player = new Player;
