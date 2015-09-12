c50f1b927a278d0bd23675b504f5f4abcad65441
NoSinging <NoSinging@users.noreply.github.com>
2015-09-12 14:44:57 +1000

    add creation test

--- spec/enemySpec.js
+++ spec/enemySpec.js
@@ -1,7 +1,15 @@
 describe('Enemy', function (){
-	it('should be able to create an enemy', function(){
-		var enemy = new Enemy();
+	it('should be able to create an enemy with row, position and sprite', function(){
+		var enemy = new Enemy(1);
 
 		expect(enemy).toBeDefined();
+
+		expect(enemy.getPosition).toBeDefined();
+
+		expect(enemy.row).toBe(1);
+
+		expect(enemy.sprite).toBeDefined();
+
 	});
+
 })
